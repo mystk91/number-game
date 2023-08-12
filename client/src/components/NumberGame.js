6557d273d23cf6d876cd48e18df8669d18838b4b
mystk91 <brischnei91@gmail.com>
2023-08-11 23:27:44 -0400

    Set up local storage system
    Set up a system using local storage and references to track inputs in the game. Still need to compare users guess when the target number.

--- client/src/components/NumberGame.js
+++ client/src/components/NumberGame.js
@@ -17,13 +17,27 @@ import { Link } from "react-router-dom";
 //import styled, { css, ThemeProvider } from 'styled-components';
 
 function NumberGame(props) {
-  const [property, setProperty] = useState("initialValue");
-  const propRef = useRef("initialValue");
-  function setPropRef(point) {
-    propRef.current = point;
-  }
 
-  const [rows, setRows] = useState([]);
+
+  //Used to update the board state visually
+  const [board, setBoard] = useState([]);
+
+  //Used to update the board state
+  const boardStateRef = useRef([]);
+  function setBoardStateRef(point) {
+    boardStateRef.current = point;
+  }
+  //Used to update the row game is being played on
+  const currentRowRef = useRef(0);
+  function setCurrentRowRef(point) {
+    currentRowRef.current = point;
+  }  
+
+  //A temporary random number used to test the game
+  const correctNumberRef = useRef(0);
+  function setCorrectNumberRef(point) {
+    correctNumberRef.current = point;
+  }  
 
   //componentDidMount, runs when component mounts, then componentDismount
   useEffect(() => {
@@ -36,9 +50,24 @@ function NumberGame(props) {
   }, []);
 
   //componentDidUpdate, runs after render
-  useEffect(() => {}, [property]);
+  //useEffect(() => {}, [property]);
 
   function setupGame() {
+    setCorrectNumberRef(Math.floor(Math.random() * Math.pow(10, props.digits)))
+    console.log(correctNumberRef.current);
+    if (localStorage.getItem(`game-` + props.digits)){
+
+    }
+    else{
+      let board = new Array(props.attempts);
+      for (let i = 0; i < props.attempts; i++){
+        board[i] = '';
+      }
+      setBoardStateRef(board);
+      updateLocalStorage();
+    }
+
+    /*
     //Creates the rows used during the game
     let rowsTemp = [];
     for (let i = 0; i < props.attempts; i++) {
@@ -46,9 +75,9 @@ function NumberGame(props) {
       for (let j = 0; j < props.digits; j++) {
         let digit = (
           <div
-            className={`digit digit-${j}`}
+            className={`digit digit-${j} ` + setColor()}
             key={"row" + i + "digit" + j}
-          ></div>
+          >{boardStateRef.current[i]}</div>
         );
         digits.push(digit);
       }
@@ -62,9 +91,23 @@ function NumberGame(props) {
       rowsTemp.push(row);
     }
     setRows(rowsTemp);
+
+    //Grabs the game state from local storage if user has attempted this puzzle previously
+    */
+
   }
 
-  //Handles pressing keys
+  //Helper function for setting the game state to the local storage
+  function updateLocalStorage(){
+    let gameState = {
+      board: boardStateRef.current,
+      currentRow: currentRowRef.current
+    }
+    localStorage.setItem(`game-` + props.digits, JSON.stringify(gameState));
+  }
+
+
+  //Handles pressing keys by using different functions
   function handleKeydown(e) {
     if (isFinite(e.key)) {
       inputNumber(e.key);
@@ -77,28 +120,81 @@ function NumberGame(props) {
     }
   }
 
-  function inputNumber(n) {}
-
-  function backspace() {}
-
-  function checkGuess() {
-    return 3927;
+  //Handles number input, also used when pressing keys
+  function inputNumber(n) {
+    if (boardStateRef.current[currentRowRef.current].length < props.digits){
+      let copy = boardStateRef.current;
+      copy[currentRowRef.current] += n;
+      setBoardStateRef(copy);
+      updateLocalStorage();
+    }
   }
 
-  function invalidGuess() {}
+  //Handles backpace key
+  function backspace() {
+    if (boardStateRef.current[currentRowRef.current].length > 0){
+      let copy = boardStateRef.current;
+      copy[currentRowRef.current] = copy[currentRowRef.current].slice(0, copy[currentRowRef.current].length - 1);
+      setBoardStateRef(copy);
+      updateLocalStorage();
+    }
+  }
 
-  function guessAnimation() {}
+  //Checks the users guess
+  function checkGuess() {
+    if (boardStateRef.current[currentRowRef.current].length == props.digits){
+      let result = checkNumber(Number(boardStateRef.current[currentRowRef.current]));
+      console.log(result);
+      if (result == 'ggggg'){
+        victory();
+      }
+      else{
+        if (currentRowRef != props.guesses - 1){
+          guessAnimation(result);
+          setCurrentRowRef(currentRowRef.current + 1);
+        }
+        else{
+          defeat();
+        }
+      }
+    }
+    else{
+      invalidGuess();
+    }
+  }
+
+  //Checks the number against the users guess, returns a string with the colors the blocks should become
+  // g - green
+  // y - yellow
+  // x - dark grey
+  function checkNumber(number) {
+    console.log('valid guess');
+  }
+
+  //Plays an animation when user has invalid length input
+  function invalidGuess() {
+    console.log('invalid guess')
+  }
+
+  //Plays an animation when the user has a valid input
+  function guessAnimation(result) {}
+
+  //Occurs when the user wins
+  function victory() {}
+
+  //Occurs when user runs out of guesses
+  function defeat() {}
 
   return (
     <main className="gameboard-container">
       <div className="gameboard">
 
         <div className='headline'>
-           <div className='instructions'>Guess from 1 - {Math.pow(10, props.digits) - 1}</div>
+           <div className='instructions'>Guess from 0 - {Math.pow(10, props.digits) - 1}</div>
            <div className="hint-caption">Hint</div>
         </div>
 
-        <div className="rows">{rows}</div>
+        <div className="rows">{board}</div>
 
         <div className="keyboard">
           <div className="number-inputs">
