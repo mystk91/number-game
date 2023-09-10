7b50024d8dff70f4862fb04754f5da603fb12e18
mystk91 <brischnei91@gmail.com>
2023-09-09 20:08:21 -0400

    Finished the Instructions
    Created an instructions modal for game that can be popped up using the Navbar component.

--- client/src/components/Instructions.js
+++ client/src/components/Instructions.js
@@ -25,9 +25,16 @@ function Instructions(props) {
     propRef.current = point;
   }
 
+  const [displayInstructions, setDisplayInstructions] = useState("");
+
   //componentDidMount, runs when component mounts, then componentDismount
   useEffect(() => {
-    return () => {};
+    setDigitAnimationTimingRef(setInterval(tickDigitAnimation, 500));
+    setFlipAnimationTimingRef(setInterval(tickFlipAnimation, 500));
+    return () => {
+      clearInterval(digitAnimationTimingRef.current);
+      clearInterval(flipAnimationTimingRef.current);
+    };
   }, []);
   //componentDidUpdate, runs after render
   useEffect(() => {}, [property]);
@@ -36,76 +43,203 @@ function Instructions(props) {
     return () => {};
   });
 
-  function closeInstructionsButton(){
- 
+  /* Hides the modal when you click outside the main box */
+  function hideInstructionsModal(e) {
+    if (e.target.classList[0] === "instructions-modal") {
+      setDisplayInstructions(" hide-instructions");
+    }
+  }
+
+  /* Hides the modal when you click on the X */
+  function hideInstructionsButton(e) {
+    setDisplayInstructions(" hide-instructions");
+  }
+
+  /* The following functions are used to animate the first digit box on the page
+  /* Used for the tickDigitAnimation */
+  const digitAnimationTimingRef = useRef();
+  function setDigitAnimationTimingRef(point) {
+    digitAnimationTimingRef.current = point;
+  }
+
+  const digitAnimationIndexRef = useRef(1);
+  function setDigitAnimationIndexRef(point) {
+    digitAnimationIndexRef.current = point;
+  }
+
+  const exampleNumber = [4, 6, 1, 8];
+
+  const [currentDigitNumber, setCurrentDigitNumber] = useState([
+    "",
+    "",
+    "",
+    "",
+  ]);
+
+  const [currentDigit, setCurrentDigit] = useState([
+    " current-digit",
+    "",
+    "",
+    "",
+  ]);
+
+  function tickDigitAnimation() {
+    if (digitAnimationIndexRef.current > 19) {
+      setDigitAnimationIndexRef(0);
+    } else if (digitAnimationIndexRef.current > currentDigitNumber.length) {
+      setDigitAnimationIndexRef(digitAnimationIndexRef.current + 1);
+    } else {
+      let newCurrentDigit = [];
+      let newCurrentNumbers = [];
+      for (let i = 0; i < currentDigitNumber.length; i++) {
+        if (i === digitAnimationIndexRef.current) {
+          newCurrentDigit.push(" current-digit");
+        } else {
+          newCurrentDigit.push("");
+        }
+      }
+      let max = Math.min(
+        digitAnimationIndexRef.current,
+        currentDigitNumber.length
+      );
+      for (let i = 0; i < max; i++) {
+        newCurrentNumbers.push(exampleNumber[i]);
+      }
+      while (newCurrentNumbers.length < currentDigitNumber.length) {
+        newCurrentNumbers.push("");
+      }
+      setCurrentDigit(newCurrentDigit);
+      setCurrentDigitNumber(newCurrentNumbers);
+      setDigitAnimationIndexRef(digitAnimationIndexRef.current + 1);
+    }
+  }
+
+  /* The following functions are used to animate the flip that occurs in examples 2/3
+  /* Used for the tickFlipAnimation */
+  const flipAnimationTimingRef = useRef();
+  function setFlipAnimationTimingRef(point) {
+    flipAnimationTimingRef.current = point;
+  }
+
+  const flipKeyframe = useRef(0);
+  function setFlipKeyframe(point) {
+    flipKeyframe.current = point;
+  }
+
+  const flipClassList = [
+    " green higher delay-3",
+    " yellow higher delay-2",
+    " grey higher delay-1",
+    " grey higher delay-0",
+    " higher",
+    " lower",
+  ];
+
+  const [currentClassList, setCurrentClassList] = useState([
+    "",
+    "",
+    "",
+    "",
+    "",
+    "",
+  ]);
+
+  function tickFlipAnimation() {
+    if (flipKeyframe.current === 3) {
+      setCurrentClassList(flipClassList);
+      setFlipKeyframe(flipKeyframe.current + 1);
+    } else if (flipKeyframe.current === 20) {
+      setCurrentClassList(["", "", "", "", "", ""]);
+      setFlipKeyframe(0);
+    } else {
+      setFlipKeyframe(flipKeyframe.current + 1);
+    }
   }
 
   return (
-    <div className="instructions-modal">
-    <div className="instructions">
-      <span className="instructions-top">
-        <button className="close-instructions" onClick={closeInstructionsButton}>X</button>
-      </span>
-      <span className="headline">How to play</span>
-      <div className="instructions-text">
-        Guess a number between 0 and {Math.pow(10, props.digits) - 1}.
+    <div
+      className={"instructions-modal" + displayInstructions}
+      onClick={(e) => hideInstructionsModal(e)}
+    >
+      <div className="instructions">
+        <span className="instructions-top">
+          <button
+            className="close-instructions"
+            onClick={(e) => hideInstructionsButton(e)}
+          >
+            X
+          </button>
+        </span>
+        <span className="headline">How to play</span>
+        <div className="instructions-text">
+          Guess the correct number in six tries.
+        </div>
+        <div className="game-example current-row">
+          <div className={"digit" + currentDigit[0]}>
+            {currentDigitNumber[0]}
+          </div>
+          <div className={"digit" + currentDigit[1]}>
+            {currentDigitNumber[1]}
+          </div>
+          <div className={"digit" + currentDigit[2]}>
+            {currentDigitNumber[2]}
+          </div>
+          <div className={"digit" + currentDigit[3]}>
+            {currentDigitNumber[3]}
+          </div>
+          <div className="hint"></div>
+        </div>
+        <div className="instructions-text">
+          Arrows tell you to guess higher or lower.
+        </div>
+        <div className="game-example previous-row">
+          <div className={"digit" + currentClassList[0]}>4</div>
+          <div className={"digit" + currentClassList[1]}>6</div>
+          <div className={"digit" + currentClassList[2]}>1</div>
+          <div className={"digit" + currentClassList[3]}>8</div>
+          <div className={"hint" + currentClassList[4]}></div>
+        </div>
+        <div className="game-example previous-row">
+          <div className={"digit" + currentClassList[0]}>4</div>
+          <div className={"digit" + currentClassList[1]}>6</div>
+          <div className={"digit" + currentClassList[2]}>1</div>
+          <div className={"digit" + currentClassList[3]}>8</div>
+          <div className={"hint" + currentClassList[5]}></div>
+        </div>
+        <hr></hr>
+        <div className="instructions-text">
+          Colors indicate the spot the digit should be in.
+        </div>
+        <hr></hr>
+        <div className="instructions-text">
+          Green digits are in the correct spot.
+        </div>
+        <div className="game-example colors-example">
+          <div className="digit green current-digit">4</div>
+          <div className="digit">6</div>
+          <div className="digit">1</div>
+          <div className="digit">8</div>
+          <div className="hint"></div>
+        </div>
+        <div className="instructions-text">
+          Yellow digits are in wrong spot.
+        </div>
+        <div className="game-example colors-example">
+          <div className="digit">4</div>
+          <div className="digit yellow current-digit">6</div>
+          <div className="digit">1</div>
+          <div className="digit">8</div>
+          <div className="hint"></div>
+        </div>
+        <div className="instructions-text">Grey digits are not used again.</div>
+        <div className="game-example colors-example">
+          <div className="digit">4</div>
+          <div className="digit">6</div>
+          <div className="digit grey current-digit">1</div>
+          <div className="digit grey current-digit">8</div>
+          <div className="hint"></div>
+        </div>
       </div>
-      <div className="game-example current-row">
-        <div className="digit">4</div>
-        <div className="digit">6</div>
-        <div className="digit">1</div>
-        <div className="digit">8</div>
-        <div className="hint"></div>
-      </div>
-      <div className="instructions-text">
-        Arrows tell you to guess higher or lower.
-      </div>
-      <div className="game-example previous-row">
-        <div className="digit green higher delay-3">4</div>
-        <div className="digit yellow higher delay-2">6</div>
-        <div className="digit grey higher delay-1">1</div>
-        <div className="digit grey higher delay-0">8</div>
-        <div className="hint higher"></div>
-      </div>
-      <div className="game-example previous-row">
-        <div className="digit green lower delay-3">4</div>
-        <div className="digit yellow lower delay-2">6</div>
-        <div className="digit grey lower delay-1">1</div>
-        <div className="digit grey lower delay-0">8</div>
-        <div className="hint lower"></div>
-      </div>
-      <hr></hr>
-      <div className="instructions-text">
-        Colors indicate the spot the digit should be in.
-      </div>
-      <hr></hr>
-      <div className="instructions-text">
-        Green digits are in the correct spot.
-      </div>
-      <div className="game-example">
-        <div className="digit green current-digit">4</div>
-        <div className="digit">6</div>
-        <div className="digit">1</div>
-        <div className="digit">8</div>
-        <div className="hint"></div>
-      </div>
-      <div className="instructions-text">Yellow digits are in wrong spot.</div>
-      <div className="game-example">
-        <div className="digit">4</div>
-        <div className="digit yellow current-digit">6</div>
-        <div className="digit">1</div>
-        <div className="digit">8</div>
-        <div className="hint"></div>
-      </div>
-      <div className="instructions-text">Grey digits are not used again.</div>
-      <div className="game-example">
-        <div className="digit">4</div>
-        <div className="digit">6</div>
-        <div className="digit grey current-digit">1</div>
-        <div className="digit grey current-digit">8</div>
-        <div className="hint"></div>
-      </div>
-    </div>
     </div>
   );
 }
--- client/src/components/Navbar.js
+++ client/src/components/Navbar.js
@@ -14,6 +14,7 @@ import "./Navbar.css";
 import "../normalize.css";
 import "../custom.css";
 import { Link } from "react-router-dom";
+import Instructions from "./Instructions";
 //import styled, { css, ThemeProvider } from 'styled-components';
 
 function Navbar(props) {
@@ -23,8 +24,11 @@ function Navbar(props) {
     propRef.current = point;
   }
 
+  const [instructions, setInstructions] = useState();
+
   //componentDidMount, runs when component mounts, then componentDismount
   useEffect(() => {
+    addInstructions();
     return () => {};
   }, []);
   //componentDidUpdate, runs after render
@@ -34,8 +38,20 @@ function Navbar(props) {
     return () => {};
   });
 
+  function addInstructions(){
+    if (!localStorage.getItem('previouslyVisited')){
+      setInstructions(<Instructions />);
+      localStorage.setItem('previouslyVisited', 'true');
+    }
+  }
+
+  function instructionsButton(){
+    setInstructions(<Instructions key={new Date()} />);
+  }
+
   return (
     <nav className="navigation-bar">
+      {instructions}
       <div className="game-modes">
         <label>Game Modes</label>
         <ul className="game-modes-list">
@@ -63,6 +79,7 @@ function Navbar(props) {
       <div className="logo">Numblr</div>
 
       <ul className="tools">
+        <li><button className="instructions-btn" onClick={instructionsButton}>?</button></li>
 
       </ul>
     </nav>
